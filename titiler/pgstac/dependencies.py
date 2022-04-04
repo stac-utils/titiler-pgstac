2dc3fdaea7c33179907188e2796e1cc67a91ef9d
vincentsarago <vincent.sarago@gmail.com>
2022-04-04 18:01:43 +0200

    lint

--- titiler/pgstac/dependencies.py
+++ titiler/pgstac/dependencies.py
@@ -78,11 +78,7 @@ def get_stac_item(pool: ConnectionPool, collection: str, item: str) -> Dict:
             )
 
             resp = cursor.fetchone()
-            if (
-                not resp
-                or "features" not in resp
-                or len(resp["features"] != 1)
-            ):
+            if not resp or "features" not in resp or len(resp["features"] != 1):
                 raise HTTPException(
                     status_code=404,
                     detail=f"No item '{item}' found in '{collection}' collection",
