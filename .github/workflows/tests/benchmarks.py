cbca1c74c687da7f7be02fd1ad740b3bdb555328
vincentsarago <vincent.sarago@gmail.com>
2023-06-08 14:26:43 +0200

    add simple benchmark

---
+++ .github/workflows/tests/benchmarks.py
@@ -0,0 +1,33 @@
+"""Benchmark items."""
+
+import os
+
+import httpx
+import pytest
+
+host = os.environ.get("HOST", "0.0.0.0")
+port = os.environ.get("PORT", "8081")
+
+
+@pytest.mark.parametrize(
+    "tile",
+    [
+        "0/0/0",
+        "1/1/1",
+        "2/2/1",
+        "3/5/0",
+        "4/5/9",
+        "5/16/5",
+        "6/43/31",
+    ],
+)
+def test_benchmark_tile(benchmark, tile, mosaic_id):
+    """Benchmark items endpoint."""
+
+    def f(input_tile):
+        return httpx.get(
+            f"http://{host}:{port}/mosaic/{mosaic_id}/tiles/{input_tile}?assets=asset"
+        )
+
+    response = benchmark(f, tile)
+    assert response.status_code == 200
