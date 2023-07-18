d996bf6c9ff8e348bf904c4ced85d8bf3fca4623
Ivica <ivica-matic@outlook.com>
2023-07-18 10:08:55 +0100

    fix: removed setting openapi_version==3.0.1

--- titiler/pgstac/main.py
+++ titiler/pgstac/main.py
@@ -36,24 +36,6 @@ settings = ApiSettings()
 
 app = FastAPI(title=settings.name, version=titiler_pgstac_version)
 
-
-def custom_openapi():
-    if app.openapi_schema:
-        return app.openapi_schema
-    openapi_schema = get_openapi(
-        title="TiTiler PgSTAC",
-        version=titiler_pgstac_version,
-        description="A lightweight STAC API implementation using PostgreSQL as a backend.",
-        routes=app.routes,
-        openapi_version="3.0.1",
-    )
-    app.openapi_schema = openapi_schema
-    return app.openapi_schema
-
-
-app.openapi = custom_openapi
-
-
 @app.on_event("startup")
 async def startup_event() -> None:
     """Connect to database on startup."""
