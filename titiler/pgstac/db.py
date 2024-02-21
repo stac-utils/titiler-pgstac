59bbc6f185909c5774776f3f5990543495624ebc
vincentsarago <vincent.sarago@gmail.com>
2024-02-21 15:08:23 -0500

    backport change from 1.2.2

--- titiler/pgstac/db.py
+++ titiler/pgstac/db.py
@@ -1,6 +1,6 @@
 """Database connection handling."""
 
-from typing import Optional
+from typing import Any, Dict, Optional
 
 from fastapi import FastAPI
 from psycopg_pool import ConnectionPool
@@ -9,11 +9,19 @@ from titiler.pgstac.settings import PostgresSettings
 
 
 async def connect_to_db(
-    app: FastAPI, settings: Optional[PostgresSettings] = None
+    app: FastAPI,
+    settings: Optional[PostgresSettings] = None,
+    pool_kwargs: Optional[Dict[str, Any]] = None,
 ) -> None:
     """Connect to Database."""
     if not settings:
         settings = PostgresSettings()
+
+    pool_kwargs = (
+        pool_kwargs
+        if pool_kwargs is not None
+        else {"options": "-c search_path=pgstac,public -c application_name=pgstac"}
+    )
 
     app.state.dbpool = ConnectionPool(
         conninfo=str(settings.database_url),
@@ -22,8 +30,12 @@ async def connect_to_db(
         max_waiting=settings.db_max_queries,
         max_idle=settings.db_max_idle,
         num_workers=settings.db_num_workers,
-        kwargs={"options": "-c search_path=pgstac,public -c application_name=pgstac"},
+        kwargs=pool_kwargs,
     )
+
+    # Make sure the pool is ready
+    # ref: https://www.psycopg.org/psycopg3/docs/advanced/pool.html#pool-startup-check
+    app.state.dbpool.wait()
 
 
 async def close_db_connection(app: FastAPI) -> None:
