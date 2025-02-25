a7b232822e745ad0bda4ec26f8547f11d4c98b28
Patricia Fricke <patricia.fricke@noaa.gov>
2025-02-25 16:12:30 +0000

    remove ssm parameters

--- titiler/pgstac/settings.py
+++ titiler/pgstac/settings.py
@@ -157,20 +157,9 @@ def RetrySettings() -> _RetrySettings:
 
 
 class RDSSettings(ApiSettings):
-    """Extended settings for AWS RDS
-
-    These parameters will override corresponding posgress settings if provided.
-
-    e.g. TITILER_PGSTAC_RDS_HOST_SSM_PARAM will pull the hostname from the given SSM parameter
-    and use that value instead of POSTGRES_HOST."""
+    """Extended settings for AWS RDS."""
 
     use_iam_auth: bool = False
-    host_ssm_param: Optional[str] = None
-    port_ssm_param: Optional[str] = None
-    user_ssm_param: Optional[str] = None
-    pass_secret_id: Optional[str] = None
-    dbname_ssm_param: Optional[str] = None
-    region_ssm_param: Optional[str] = None
 
     model_config = {
         "env_prefix": "TITILER_PGSTAC_RDS_",
